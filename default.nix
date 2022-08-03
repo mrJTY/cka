1d000d661814ab17525ae889533d780fe30ea667
jtdv01 <jtdv01@gmail.com>
2022-08-04 08:34:20 +1000

    Add defaults

---
+++ default.nix
@@ -0,0 +1,12 @@
+# To avtivate:
+# $ nix-build
+# $ nix-shell --pure
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.vagrant
+  ];
+  shellHook = ''
+    EDITOR=nvim
+  '';
+}
