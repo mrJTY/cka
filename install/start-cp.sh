512afd3cc9620a15202396e210149daa54e3ea30
jtdv01 <jtdv01@gmail.com>
2022-08-04 20:01:05 +1000

    devdan

--- install/start-cp.sh
+++ install/start-cp.sh
@@ -1,8 +1,11 @@
 
-sudo kubeadm init --config ./kubeadmn-config.yaml
+sudo kubeadm init --config ./kubeadm-config.yaml
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f ./calico.yaml
+
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
